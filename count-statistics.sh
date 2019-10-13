7dcfe6080358ab226a150f0b60d9d836ead60770
dansman805 <dansman805@gmail.com>
2019-10-13 12:30:21 -0400

    Optimized statistic counter

--- count-statistics.sh
+++ count-statistics.sh
@@ -4,23 +4,25 @@ add_numbers='{a+=$1}END{print a}'
 
 EXT="rst"
 
+files=`find . -type f -name "*.$EXT"`
+
 # Print the word count of all the .$EXT files recursively
 printf "Word Count: "
-for file in `find . -type f -name "*.$EXT"`;
+for file in $files;
 do
     wc -w "$file"
 done | awk "$add_numbers"
 
 # Print the line count of all the .$EXT files recursively
 printf "Line Count: "
-for file in `find . -type f -name "*.$EXT"`;
+for file in $files;
 do
     wc -l "$file"
 done | awk "$add_numbers"
 
 # Print the character count of all the .$EXT files recursively
 printf "Character Count: "
-for file in `find . -type f -name "*.$EXT"`;
+for file in $files;
 do
     wc -m "$file"
 done | awk "$add_numbers"
