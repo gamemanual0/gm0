fd452c28dee38a0763f372214eed8cbad384e7d9
dansman805 <dansman805@gmail.com>
2020-07-10 22:29:40 -0400

    Change ogp to use github hosted image instead of self hosted one

--- source/conf.py
+++ source/conf.py
@@ -65,7 +65,7 @@ hoverxref_roles = ['term']
 # See https://github.com/wpilibsuite/sphinxext-opengraph
 
 ogp_site_url = "https://gm0.copperforge.cc/en/latest/"
-ogp_image = "https://gm0.copperforge.cc/en/latest/_static/gm0-logo.png"
+ogp_image = "https://raw.githubusercontent.com/Coppersource/gm0/master/source/assets/gm0-logo.png"
 ogp_site_name = "Game Manual 0"
 
 # Enables ChiefDelphi support
