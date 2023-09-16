3bd4590003254d1b2447fc70a5bf66ac5697854d
Abigail <abigail@dogbuilt.net>
2023-09-16 01:41:21 -0400

    Add a modicum of error-handling

--- source/_static/calculator-utils.js
+++ source/_static/calculator-utils.js
@@ -3,9 +3,27 @@ function prettifyFloat(float) {
 }
 
 function writeText(id, content) {
+    if (isNaN(content)) {
+      content = "Invalid!";
+    }
+
     document.getElementById(id).innerText = content;
 }
 
 function getValue(id) {
-    return document.getElementById(id).value;
+    const value = parseFloat(document.getElementById(id).value);
+
+    if (isNaN(value)) {
+        throw new Error("Value must be a number!");
+    }
+
+    if (value <= 0) {
+        throw new Error("Value must be positive!");
+    }
+
+    if (value >= 1e6) {
+        throw new Error("Value must be reasonably small! (Less than 1e6.)");
+    }
+
+    return value;
 }
