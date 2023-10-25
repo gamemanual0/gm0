e76752ff064145888d78e7705b4b3608f27794f7
Abigail <abigail@dogbuilt.net>
2023-10-25 01:51:59 -0400

    Clean up comments

--- scripts/rtd-compress-pdf.sh
+++ scripts/rtd-compress-pdf.sh
@@ -5,18 +5,20 @@
 DPI=400
 PDF_DIR="$READTHEDOCS_OUTPUT/pdf"
 
-# Check if the PDF build is enabled based on if the PDF output directory exists
+# only run if the PDF build is enabled
+# based on whether or not the PDF output directory exists
 if [ -d "$PDF_DIR" ]; then
-    # find the already built PDF
+    # find the existing PDF
     pdf_name=$(ls "$PDF_DIR")
 
-    # move it into a temp directory
-    # readthedocs expects only one PDF output in the directory
+    # Read the Docs expects only one PDF file in the output directory
     # see https://docs.readthedocs.io/en/stable/reference/environment-variables.html#envvar-READTHEDOCS_OUTPUT
+    # so we move the existing PDF into a temp directory.
     temp=$(mktemp -d)
     mv "$PDF_DIR/$pdf_name" "$temp"
 
-    # actually compress the PDF
+    # compress the existing PDF into a new PDF,
+    # placed directly in the output directory
     gs -sDEVICE=pdfwrite \
         -dCompatibilityLevel=1.4 \
         -dPDFSETTINGS=/ebook \
