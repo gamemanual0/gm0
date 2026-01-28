995bb2daab99d90062dae097bd3525d70af8ab40
Mikhail Lavelin <59438110+mikhaillav@users.noreply.github.com>
2026-01-29 01:57:59 +0300

    Added macroses for Competition Manual and Engineering Portfolio

--- source/conf.py
+++ source/conf.py
@@ -61,6 +61,8 @@ rst_epilog = """
 .. |gm2| replace:: Game Manual Part 2
 .. |cm| replace:: Competition Manual
 .. |EN| replace:: Engineering Notebook
+.. |CM| replace:: Competition Manual
+.. |EP| replace:: Engineering Portfolio
 """
 
 # Disable following anchors in URLs for linkcheck.
